4bca7ff88121204e64ec46697bb5ab8f2c1cbd9e
shenjizhe <thirdlucky@126.com>
2020-03-16 22:11:13 +0800

    spark 作为 kafka的 消费者单词统计

--- spark-demo/src/main/scala/com/thirdlucky/sparkstreaming/KafkaCountStreamingObject.scala
+++ spark-demo/src/main/scala/com/thirdlucky/sparkstreaming/KafkaCountStreamingObject.scala
@@ -1,32 +1,44 @@
 package com.thirdlucky.sparkstreaming
 
-import org.apache.spark.streaming.dstream.{DStream, ReceiverInputDStream}
-import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.dstream.{DStream}
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
 
 object KafkaCountStreamingObject {
   def main(args: Array[String]): Unit = {
     import org.apache.spark.SparkConf
     import org.apache.spark.streaming.{Seconds, StreamingContext}
-    val conf = new SparkConf().setAppName("spark-stream-test").setMaster("local[*]")
-    val streaming = new StreamingContext(conf,Seconds(3))
+    val conf = new SparkConf().setAppName("spark-stream-kafka").setMaster("local[*]")
+    val streamingContext = new StreamingContext(conf, Seconds(3))
 
-    val receiver: ReceiverInputDStream[(String, String)] = KafkaUtils.createStream(
-      streaming,
-      "192.268.227.135:2181", // zookeeper 的 host 和 port
-      "spark-group", // 消息分组
-      Map("spark-topic" -> 3)
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "hadoop-node1:9092,hadoop-node2:9092,hadoop-node3:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "spark-group",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
     )
 
-    val wordRDD: DStream[String] = receiver.flatMap(_._2.split(" "))
+    val topics = Array("spark-topic")
+
+    val stream = KafkaUtils.createDirectStream[String, String](
+      streamingContext,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+    val wordRDD: DStream[String] = stream.flatMap(_.value.split(" "))
     val tupleRDD: DStream[(String, Int)] = wordRDD.map((_, 1))
     val wordCount: DStream[(String, Int)] = tupleRDD.reduceByKey(_ + _)
 
     wordCount.print()
-    //TODO: 需要先学习 zookeeper kafka flume
 
     // 启动采集器
-    streaming.start()
+    streamingContext.start()
     // Driver 等待采集器执行
-    streaming.awaitTermination()
+    streamingContext.awaitTermination()
   }
 }
