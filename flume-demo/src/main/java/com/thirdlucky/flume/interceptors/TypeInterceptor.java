caa80b6f966bccedd9b6b76770c2523abd865d69
shenjizhe <thirdlucky@126.com>
2020-03-16 10:35:27 +0800

    kafka 拦截器，自动分主题

--- flume-demo/src/main/java/com/thirdlucky/flume/interceptors/TypeInterceptor.java
+++ flume-demo/src/main/java/com/thirdlucky/flume/interceptors/TypeInterceptor.java
@@ -3,6 +3,8 @@ package com.thirdlucky.flume.interceptors;
 import org.apache.flume.Context;
 import org.apache.flume.Event;
 import org.apache.flume.interceptor.Interceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
@@ -11,16 +13,21 @@ import java.util.Map;
  * @author Json Shen
  */
 public class TypeInterceptor implements Interceptor {
+    private static final Logger logger = LoggerFactory
+            .getLogger(TypeInterceptor.class);
     @Override
     public void initialize() {
+        logger.info("自定义拦截器初始化");
     }
 
     @Override
     public Event intercept(Event event) {
 
+
         Map<String, String> headers = event.getHeaders();
 
         String body = new String(event.getBody());
+        logger.info("自定义拦截器执行"+body);
         if (body.contains("hello")) {
             headers.put("topic", "first");
         } else {
@@ -43,12 +50,14 @@ public class TypeInterceptor implements Interceptor {
     public static class Builder implements Interceptor.Builder{
         @Override
         public Interceptor build() {
+            logger.info("创建拦截器");
             return new TypeInterceptor();
         }
 
         @Override
         public void configure(Context context) {
-
+            String param = context.getString("param");
+            logger.info("----------configure方法执行参数：" + param);
         }
     }
 }
